75ddb23d60879cd999c523a078b9161b57119522
Goose <josephtrop@gmail.com>
2017-09-09 13:22:47 -0700

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,20 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import {Navbar, NavItem, Row, Col, Input, Carousel} from "react-materialize";
+import { Button } from 'antd';
+
 
 class App extends Component {
 
-  state = {users: []};
-
-  componentDidMount() {
-    fetch("/123").then(res => res.json()).then(users => this.setState({users}));
-  }
-
   render() {
-    console.log("Rendering");
-    console.log(`this.state = ${this.state.user}`);
     return (
       <div className="App">
-        <Row>
-          <Col m={6}>A</Col>
-          <Col m={6}>B</Col>
-        </Row>
-        <h1>Users</h1>
-        {this.state.users.map(user => 
-          <div key={user.id}>{user.name}</div>
-        )}
+        <div>
+          <Button type="primary">Primary</Button>
+          <Button>Default</Button>
+          <Button type="dashed">Dashed</Button>
+          <Button type="danger">Danger</Button>
+        </div>
       </div>
     );
   }
