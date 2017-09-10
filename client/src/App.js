6d9cb32b2064dec49398aa1140def5af67798694
Goose <josephtrop@gmail.com>
2017-09-09 17:35:40 -0700

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
-import Homepage from './components/Homepage'
+
 
 
 class App extends Component {
