d12768e8f19f447ce1df52fafc8e61a177602151
ah98607 <adh6107@gmail.com>
2017-09-12 02:23:06 -0700

    added shared footer, top-navbar; started designing homepage; added some css files

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import React, {Component} from "react";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Home from './components/home/'
-import Navpills from './components/shared/navpills'
+import Home from './components/home/';
+import Navpills from './components/shared/navpills';
+import "./App.css";
 
 class App extends Component {
     render() {
--- client/src/components/shared/footer/index.js
+++ client/src/components/shared/footer/index.js
@@ -1,11 +1,20 @@
-import React , { Component } from 'react'
-import './style.css'
+import React , { Component } from 'react';
+import './style.css';
 
 class Bottom extends Component {
     render(){
         return(
-            <div className="footer">
-                Bottom
+            <div>
+                <hr />
+                <div className="footer">
+                    <div>
+                        <span className="footer-link"><a href="https://www.google.com">About Us</a></span>
+                        <span className="footer-link"><a href="https://www.google.com">API Category</a></span>
+                    </div>
+                    <div className="copyright-statement">
+                        XXX Copyright Reserved
+                    </div>
+                </div>
             </div>
         )
     }
