5522775a427953b8b912b91f61418c38d864d1a1
Goose <josephtrop@gmail.com>
2017-09-11 20:50:29 -0700

    NEWEST DEV

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, {Component} from "react";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 
 import Home from './components/home/'
-import Alljobs from './components/alljobs/'
 import Navpills from './components/shared/navpills'
 
 class App extends Component {
@@ -13,7 +12,7 @@ class App extends Component {
                 <div>
                     <Navpills/>
                     <Route exact path="/" component={Home}/>
-                    <Route path="/alljobs" component={Alljobs}/>
+
                 </div>
             </Router>
         );
--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,20 +1,12 @@
 import React , { Component } from 'react'
 
-import {Navbar, NavItem, Row, Col, Input, Carousel} from "react-materialize"
+
 
 class Homepage extends Component{
     render(){
         return(
             <div>
-                <Navbar brand='logo' left>
-                    <NavItem href='get-started.html'>Explore Jobs</NavItem>
-                    <NavItem href='get-started.html'>Post a Job</NavItem>
-                    <h1>
-                        Homepage - to be designed
-                    </h1>
-                    <NavItem href='components.html'>Components</NavItem>
-                </Navbar>
-                
+               Homepage
             </div>
         )
     }
