ba729c51ec83fe2357d53f89ffeb1a8368a2b78a
Shirlon Banks <sssbanks@gmail.com>
2017-09-11 21:22:39 -0700

    update profile

---
+++ client/src/components/profile/index.js
@@ -0,0 +1,17 @@
+import React , { Component } from 'react'
+import
+
+
+class Profile extends Component{
+    render(){
+        return(
+            <div>
+              <h1>PROFILE</h1>
+
+        	</div>
+
+        )
+    }
+}
+
+export default  Profile
