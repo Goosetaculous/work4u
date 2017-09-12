671470fcfdc04f4b49b8fbd4bf33489ea5da7c40
ah98607 <adh6107@gmail.com>
2017-09-11 20:34:29 -0700

    removal of container

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from "react";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 
-import Home from './components/home/Homepage'
+import Home from './components/home/'
 import Alljobs from './components/alljobs/'
 import Navpills from './components/shared/navpills'
 
