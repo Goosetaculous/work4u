39fd7b3bf990ec135b63720dbfb28b81a006a813
ah98607 <adh6107@gmail.com>
2017-09-13 00:09:25 -0700

    added more stuff to landing page

--- client/src/components/home/HomePageJobList.js
+++ client/src/components/home/HomePageJobList.js
@@ -7,77 +7,70 @@ const styles = {
   root: {
     display: 'flex',
     flexWrap: 'wrap',
+    justifyContent: 'space-around',
   },
   gridList: {
+    overflowX: 'auto',
+    flexWrap: 'nowrap',
+    width: '80%',
   },
   titleStyle: {
-    color: 'rgb(0, 188, 212)',
+    textalign: 'center',
+    color: 'orange',
+    fontSize: '30px',
   },
 };
 
 const tilesData = [
   {
-    img: 'https://d30y9cdsu7xlg0.cloudfront.net/png/466667-200.png',
+    img: 'https://d30y9cdsu7xlg0.cloudfront.net/png/324638-200.png',
     title: 'Breakfast',
     author: 'jill111',
   },
   {
-    img: 'https://d30y9cdsu7xlg0.cloudfront.net/png/466667-200.png',
+    img: 'https://d30y9cdsu7xlg0.cloudfront.net/png/324638-200.png',
     title: 'Tasty burger',
     author: 'pashminu',
   },
   {
-    img: 'https://d30y9cdsu7xlg0.cloudfront.net/png/466667-200.png',
+    img: 'https://d30y9cdsu7xlg0.cloudfront.net/png/324638-200.png',
     title: 'Camera',
     author: 'Danson67',
   },
   {
-    img: 'https://d30y9cdsu7xlg0.cloudfront.net/png/466667-200.png',
+    img: 'https://d30y9cdsu7xlg0.cloudfront.net/png/324638-200.png',
     title: 'Morning',
     author: 'fancycrave1',
   },
   {
-    img: 'https://d30y9cdsu7xlg0.cloudfront.net/png/466667-200.png',
+    img: 'https://d30y9cdsu7xlg0.cloudfront.net/png/324638-200.png',
     title: 'Hats',
     author: 'Hans',
   },
   {
-    img: 'https://d30y9cdsu7xlg0.cloudfront.net/png/466667-200.png',
+    img: 'https://d30y9cdsu7xlg0.cloudfront.net/png/324638-200.png',
     title: 'Honey',
     author: 'fancycravel',
-  },
-  {
-    img: 'https://d30y9cdsu7xlg0.cloudfront.net/png/466667-200.png',
-    title: 'Vegetables',
-    author: 'jill111',
-  },
-  {
-    img: 'https://d30y9cdsu7xlg0.cloudfront.net/png/466667-200.png',
-    title: 'Water plant',
-    author: 'BkrmadtyaKarki',
   },
 ];
 
 /**
  * This example demonstrates the horizontal scrollable single-line grid list of images.
  */
-const HomePageGridList = () => (
+const HomePageJobList = () => (
   <div style={styles.root}>
-    12345
     <GridList style={styles.gridList} cols={2.2}>
       {tilesData.map((tile) => (
         <GridTile
-          key={tile.img}
           title={tile.title}
           actionIcon={<IconButton><StarBorder color="rgb(0, 188, 212)" /></IconButton>}
           titleStyle={styles.titleStyle}
           titleBackground="linear-gradient(to top, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0.3) 70%,rgba(0,0,0,0) 100%)"
         >
-          <img src={tile.img} />
+          <img src={tile.img} width="20px"/>
         </GridTile>
       ))}
     </GridList>
   </div>
 );
-
-export default HomePageGridList;
+export default HomePageJobList;
