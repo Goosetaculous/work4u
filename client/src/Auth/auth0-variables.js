3e02319bc9f257bfaa7764ffa21f9ed04b8f1230
Goose <josephtrop@gmail.com>
2017-09-21 12:25:10 -0700

    updates

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,10 +1,10 @@
 
 
-console.log(process.env.CB_URL || 'http://localhost:3000/callback')
+console.log(process.env.AUTHO_CB_URL || 'http://localhost:3000/callback')
 export const AUTH_CONFIG = {
   domain: 'jonathanthinh.auth0.com',
   clientId: 'Y1J7wHC7JktSj70xG0opZEi4rN7F24tg',
-    callbackUrl: process.env.CB_URL || 'http://localhost:3000/callback'
+    callbackUrl: process.env.AUTHO_CB_URL || 'http://localhost:3000/callback'
 }
 
 
