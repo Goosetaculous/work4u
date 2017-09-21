1b479e7d4bcfe90b1b9c78db39f4813cd7ddb50d
Goose <josephtrop@gmail.com>
2017-09-20 20:47:20 -0700

    updates

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,15 +1,15 @@
-var callbackUrl
+var cbURL
 if (process.env.NODE_ENV === "production"){
-    callbackUrl='https://work4u.herokuapp.com/callback'
+    cbURL='https://work4u.herokuapp.com/callback'
 
 }else if(process.env.NODE_ENV === "development"){
-    callbackUrl='https://work4u-dev.herokuapp.com/callback'
+    cbURL='https://work4u-dev.herokuapp.com/callback'
 
 }else {
-    callbackUrl='http://localhost:3000/callback'
+    cbURL='http://localhost:3000/callback'
 }
 export const AUTH_CONFIG = {
   domain: 'jonathanthinh.auth0.com',
   clientId: 'Y1J7wHC7JktSj70xG0opZEi4rN7F24tg',
-        callbackUrl
+    callbackUrl: cbURL
 }
