bdbd75435358179ed2c60d9d1422836fdbcadf05
ah98607 <adh6107@gmail.com>
2017-09-20 21:06:47 -0700

    checks if a user already exists

--- controllers/UserModelController.js
+++ controllers/UserModelController.js
@@ -13,22 +13,42 @@ var UserModelController = {
 
 
     add: function(req, res) {
-        var userObj = {
-            firstName: "jon" ,
-            lastName: "doe",
+
+        console.log("DB qeuery initiated.");
+        /*var userObj = {
+            given_name: "jon" ,
+            family_name: "doe",
+            name: "jon doe",
+            user_id: "google123",
             skills: [],
             jobsPostedByThisUser: [] ,
-            jobsThisUserApplied:[],
-            auth0Id: "google123"
+            jobsThisUserApplied:[]
+            
         };
-        console.log(req.body)
+        console.log(userObj);*/
 
-        //create custom user object - to be added//
+        // first, check if the user has already been added
+        //UserModel.findOne({user_id: userObj.user_id}, function(err, data) {
+        UserModel.findOne({user_id: req.body.user_id}, function(err, data) {
 
-        UserModel.create(userObj).then(function(doc) {
-            res.json(doc);
+            console.log("123321");
+            console.log(data);
+            
+            if (!data) {
+                console.log(userObj);
+                console.log("User has not been created before. Now storing it to DB.");
+                UserModel.create(userObj).then(function(doc) {
+                    console.log("creating finished");
+                    res.json(doc);
+                }).catch(function(err) {
+                    res.json(err);
+                });
+            }
+            else {
+                console.log("User has already been created before.")
+            }
         }).catch(function(err) {
-            res.json(err);
+            res.json(err)
         });
     },
 
@@ -38,7 +58,7 @@ var UserModelController = {
         var user = req.body.user_id;
 
         UserModel.update(
-            {auth0Id: user},
+            {user_id: user},
             {$push: {jobsPostedByThisUser: newPost}},
             req.body
         ).then(function(doc) {
@@ -54,7 +74,7 @@ var UserModelController = {
         var user = req.body.user_id;
 
         UserModel.update(
-            {auth0Id: user},
+            {user_id: user},
             {$push: {jobsThisUserApplied: newPost}},
             req.body
         ).then(function(doc) {
