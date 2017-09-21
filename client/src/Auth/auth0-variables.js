32c7c0d55fdec524e66a0a99097f26691f64eea4
Joseph "Goose" Aranez <josephtrop@gmail.com>
2017-09-21 12:41:41 -0700

    Update auth0-variables.js
    using base url

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,11 +1,5 @@
-
-console.log(process.env.AUTHO_CB_URL)
-console.log(process.env.AUTHO_CB_URL || 'http://localhost:3000/callback')
 export const AUTH_CONFIG = {
   domain: 'jonathanthinh.auth0.com',
   clientId: 'Y1J7wHC7JktSj70xG0opZEi4rN7F24tg',
-    callbackUrl: process.env.AUTHO_CB_URL || 'http://localhost:3000/callback'
+    callbackUrl: window.location.origin+"/callback"
 }
-
-
-
