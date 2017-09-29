46cd8cf3945be6c49acdb1a81850109898e14a66
Goose <josephtrop@gmail.com>
2017-09-29 15:47:56 -0700

    playing with jobs posted by me

--- client/src/components/profile/tabs/JobsPostedByMe.js
+++ client/src/components/profile/tabs/JobsPostedByMe.js
@@ -1,7 +1,6 @@
 import React ,  { Component } from 'react'
-import {GridList, GridTile} from 'material-ui/GridList';
-import FlatButton from 'material-ui/FlatButton';
 import API from '../../../utils/API.js';
+import PopulateCards from '../../shared/3buttoncards';
 
 const styles = {
     root: {
@@ -11,56 +10,9 @@ const styles = {
     },
     gridList: {
         width: 1000,
-
         overflowY: 'auto',
     },
 };
-
-/*const tilesData = [
-    {
-        img: 'https://pixy.org/images/placeholder.png',
-        title: 'Mow my Law',
-        author: 'UCSD',
-        featured: true,
-    },
-    {
-        img: 'https://pixy.org/images/placeholder.png',
-        title: 'Clean my car',
-        author: 'Carlsbad',
-    },
-    {
-        img: 'images/grid-list/camera-813814_640.jpg',
-        title: 'Camera',
-        author: 'Danson67',
-    },
-    {
-        img: 'images/grid-list/morning-819362_640.jpg',
-        title: 'Morning',
-        author: 'fancycrave1',
-        featured: true,
-    },
-    {
-        img: 'images/grid-list/hats-829509_640.jpg',
-        title: 'Hats',
-        author: 'Hans',
-    },
-    {
-        img: 'images/grid-list/honey-823614_640.jpg',
-        title: 'Honey',
-        author: 'fancycravel',
-    },
-    {
-        img: 'images/grid-list/vegetables-790022_640.jpg',
-        title: 'Vegetables',
-        author: 'jill111',
-    },
-    {
-        img: 'images/grid-list/water-plant-821293_640.jpg',
-        title: 'Water plant',
-        author: 'BkrmadtyaKarki',
-    },
-];*/
-
 
 class JobsPostedByMe extends Component {
 
@@ -119,42 +71,54 @@ class JobsPostedByMe extends Component {
     render(){
         return(
             <div style={styles.root}>
-                <GridList
-                    cellHeight={180}
-                    style={styles.gridList}
-                    cols={4}
-                    padding={3}
-                >
-                    {this.state.jobs.map((job) => {
-                        if (job.status == "applied" ) {
-                            return <GridTile
-                                        title={job.jobName}
-                                        titlePosition="top"
-                                        subtitle={job.location}
-                                        actionIcon={
-                                            <div>
-                                                <FlatButton label="Stop Posting" backgroundColor="#F53F30" primary={true} onClick={(event) => this.removeJobByIdAndRemoveApplicationById(event, job._id, job.appliedBy)}/>
-                                                <FlatButton label="Confirm" backgroundColor="#F53F30" primary={true} onClick={(event) => this.confirmJobById(event, job._id)}/>
-                                                <FlatButton label="Decline" backgroundColor="#F53F30" primary={true} onClick={(event) => this.declineApplicantById(event, job._id, job.appliedBy)}/>
-                                            </div>}
-                                    >
-                                    </GridTile>
-                        }
-                        else if (job.status == "initiated") {
-                            return <GridTile
-                                        title={job.jobName}
-                                        titlePosition="top"
-                                        subtitle={job.location}
-                                        actionIcon={<FlatButton label="Stop Posting" backgroundColor="#F53F30" primary={true} onClick={(event) => this.removeOnlyJobById(event, job._id)}/>}
-                                    >
-                                    </GridTile>
-                        }
-                        else {
-                            return <div></div>;
-                        }
-                        
-                    })}
-                </GridList>
+                {/*<GridList*/}
+                    {/*cellHeight={180}*/}
+                    {/*style={styles.gridList}*/}
+                    {/*cols={4}*/}
+                    {/*padding={3}*/}
+                {/*>*/}
+                    <PopulateCards
+                        jobs={this.state.jobs}
+                        removeJobByIdAndRemoveApplicationById={this.removeJobByIdAndRemoveApplicationById}
+                        removeOnlyJobById={this.removeOnlyJobById}
+                        confirmJobById={this.confirmJobById}
+                        declineApplicantById={this.declineApplicantById}
+
+
+                    />
+
+                    {/*{this.state.jobs.map((job) => {*/}
+                       {/**/}
+
+                        {/*if (job.status == "applied" ) {*/}
+                            {/*return <GridTile*/}
+                                        {/*title={job.jobName}*/}
+                                        {/*titlePosition="top"*/}
+                                        {/*subtitle={job.location}*/}
+                                        {/*actionIcon={*/}
+                                            {/*<div>*/}
+                                                {/*<FlatButton label="Stop Posting" backgroundColor="#F53F30" primary={true} onClick={(event) => this.removeJobByIdAndRemoveApplicationById(event, job._id, job.appliedBy)}/>*/}
+                                                {/*<FlatButton label="Confirm" backgroundColor="#F53F30" primary={true} onClick={(event) => this.confirmJobById(event, job._id)}/>*/}
+                                                {/*<FlatButton label="Decline" backgroundColor="#F53F30" primary={true} onClick={(event) => this.declineApplicantById(event, job._id, job.appliedBy)}/>*/}
+                                            {/*</div>}*/}
+                                    {/*>*/}
+                                    {/*</GridTile>*/}
+                        {/*}*/}
+                        {/*else if (job.status == "initiated") {*/}
+                            {/*return <GridTile*/}
+                                        {/*title={job.jobName}*/}
+                                        {/*titlePosition="top"*/}
+                                        {/*subtitle={job.location}*/}
+                                        {/*actionIcon={<FlatButton label="Stop Posting" backgroundColor="#F53F30" primary={true} onClick={(event) => this.removeOnlyJobById(event, job._id)}/>}*/}
+                                    {/*>*/}
+                                    {/*</GridTile>*/}
+                        {/*}*/}
+                        {/*else {*/}
+                            {/*return <div></div>;*/}
+                        {/*}*/}
+                        {/**/}
+                    {/*})}*/}
+                {/*</GridList>*/}
             </div>
 
         )
---
+++ client/src/components/shared/3buttoncards/index.js
@@ -0,0 +1,70 @@
+import React ,  { Component } from 'react'
+import {GridList, GridTile} from 'material-ui/GridList';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+
+//API
+import API from '../../../utils/API'
+
+
+
+const styles = {
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+    },
+    gridList: {
+        width: 1000,
+        overflowY: 'auto',
+        padding: '2px'
+    },
+};
+
+
+class PopulateCards extends Component {
+    constructor(){
+        super()
+        this.state={
+            applied: true
+        }
+    }
+
+   
+    createCard(job){
+        return(
+            <Card>
+                <CardText>
+                    <b>Job</b>: {job.jobName}
+                    <br/>
+                    <b>Location</b>: {job.location}
+                </CardText>
+                <CardActions>
+                    <FlatButton label="Stop Posting" secondary={true} />
+                    <FlatButton label="Confirm" primary={true} />
+                    <FlatButton label="Decline" primary={true} />
+                </CardActions>
+            </Card>
+        )
+
+    }
+
+    render(){
+        return(
+            <div style={styles.root}>
+                <GridList
+                    cellHeight={180}
+                    style={styles.gridList}
+                    cols={4}
+                    padding={3}
+                >
+                    {this.props.jobs.length > 0? this.props.jobs.map((job) => this.createCard(job) ):<div>No Jobs found</div> }
+                </GridList>
+            </div>
+
+        )
+    }
+
+}
+
+export default PopulateCards
