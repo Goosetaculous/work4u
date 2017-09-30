1582032102c620673fb3a160777cc5c3e9760557
Jonathan Thinh <jonathan.thinh@gmail.com>
2017-09-29 21:28:44 -0700

    recommended jobs refactor

--- client/src/components/profile/tabs/RecommendedJobs.js
+++ client/src/components/profile/tabs/RecommendedJobs.js
@@ -2,7 +2,22 @@ import React ,  { Component } from 'react'
 import {GridList, GridTile} from 'material-ui/GridList';
 import FlatButton from 'material-ui/FlatButton';
 
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+
 import API from '../../../utils/API'
+
+// const styles = {
+//     root: {
+//         display: 'flex',
+//         flexWrap: 'wrap',
+//         justifyContent: 'space-around',
+//     },
+//     gridList: {
+//         width: 1000,
+
+//         overflowY: 'auto',
+//     },
+// };
 
 const styles = {
     root: {
@@ -12,12 +27,10 @@ const styles = {
     },
     gridList: {
         width: 1000,
-
         overflowY: 'auto',
+        padding: '2px'
     },
 };
-
-
 
 
 
@@ -25,8 +38,8 @@ const styles = {
 class RecommededJobs extends Component {
     constructor(){
         super()
-        this.state = {
-            // user_id: this.props.user_id
+        this.state={
+            applied: true
         }
     }
 
@@ -43,6 +56,32 @@ class RecommededJobs extends Component {
     }
 
 
+  
+
+createCard(job){
+        return(
+            <Card>
+
+                <CardHeader
+                    title={`${job.jobName} in ${job.location}`}
+                   
+                />
+                <CardMedia>
+                      
+                      <img src={`${job.image_url}`} alt="" />
+                </CardMedia>
+
+                <CardText>
+                    som job description
+                </CardText>
+                <CardActions>
+                    <FlatButton label="Apply" onClick={(event) => this.applyToJob(event, job)} />
+                </CardActions>
+            </Card>
+        )
+
+    }
+
     render(){
         return(
             <div style={styles.root}>
@@ -52,31 +91,51 @@ class RecommededJobs extends Component {
                     cols={4}
                     padding={3}
                 >
-                    {this.props.recommendedJobs.map((job) => ( 
-                        
-                            <GridTile
-                                title={job.jobName}
-                                titlePosition="top"
-                                subtitle={job.postedBy}
-                                actionIcon={
-                                    <FlatButton 
-                                        label="Apply" 
-                                        backgroundColor="green" 
-                                        color="white" 
-                                        primary={true} 
-                                        onClick={(event) => this.applyToJob(event, job)}
-                                    />}                                
-                            >
-                            </GridTile>
-
-                        
-                    ))}
+                    {this.props.recommendedJobs.length > 0? this.props.recommendedJobs.map((job) => this.createCard(job) ):<div>No Jobs found</div> }
                 </GridList>
             </div>
 
         )
     }
 
+
+    //   render(){
+    //     return(
+    //         <div style={styles.root}>
+    //             <GridList
+    //                 cellHeight={180}
+    //                 style={styles.gridList}
+    //                 cols={4}
+    //                 padding={3}
+    //             >
+    //                 {this.props.recommendedJobs.map((job) => ( 
+                        
+    //                         <GridTile
+    //                             title={job.jobName}
+    //                             titlePosition="top"
+    //                             subtitle={job.postedBy}
+    //                             actionIcon={
+    //                                 <FlatButton 
+    //                                     label="Apply" 
+    //                                     backgroundColor="green" 
+    //                                     color="white" 
+    //                                     primary={true} 
+    //                                     onClick={(event) => this.applyToJob(event, job)}
+    //                                 />}                                
+    //                         >
+    //                         </GridTile>
+
+                        
+    //                 ))}
+    //             </GridList>
+    //         </div>
+
+    //     )
+    // }
+
 }
+
+
+
 
 export default RecommededJobs;
