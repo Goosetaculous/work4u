fb3b16e95920c2bd0879d145d64bf724ada55aff
Jonathan Thinh <jonathan.thinh@gmail.com>
2017-09-29 23:26:58 -0700

    all tabs refactored to use cards / apply completed

--- client/src/components/profile/tabs/ConfirmedJobs.js
+++ client/src/components/profile/tabs/ConfirmedJobs.js
@@ -3,6 +3,9 @@ import {GridList, GridTile} from 'material-ui/GridList';
 import FlatButton from 'material-ui/FlatButton';
 import API from '../../../utils/API.js';
 
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+
+
 const styles = {
     root: {
         display: 'flex',
@@ -12,53 +15,10 @@ const styles = {
     gridList: {
         width: 1000,
         overflowY: 'auto',
+        padding: '2px'
     },
 };
 
-/*const tilesData = [
-    {
-        img: 'https://pixy.org/images/placeholder.png',
-        title: 'Mow my Law',
-        author: 'UCSD',
-        featured: true,
-    },
-    {
-        img: 'https://pixy.org/images/placeholder.png',
-        title: 'Clean my car',
-        author: 'Carlsbad',
-    },
-    {
-        img: 'images/grid-list/camera-813814_640.jpg',
-        title: 'Camera',
-        author: 'Danson67',
-    },
-    {
-        img: 'images/grid-list/morning-819362_640.jpg',
-        title: 'Morning',
-        author: 'fancycrave1',
-        featured: true,
-    },
-    {
-        img: 'images/grid-list/hats-829509_640.jpg',
-        title: 'Hats',
-        author: 'Hans',
-    },
-    {
-        img: 'images/grid-list/honey-823614_640.jpg',
-        title: 'Honey',
-        author: 'fancycravel',
-    },
-    {
-        img: 'images/grid-list/vegetables-790022_640.jpg',
-        title: 'Vegetables',
-        author: 'jill111',
-    },
-    {
-        img: 'images/grid-list/water-plant-821293_640.jpg',
-        title: 'Water plant',
-        author: 'BkrmadtyaKarki',
-    },
-];*/
 
 
 class ConfirmedJobs extends Component {
@@ -99,40 +59,114 @@ class ConfirmedJobs extends Component {
         });
     };
 
+
+
+createCard(job){
+    if(job.status == "confirmed"){
+        return(
+             <Card>
+                <CardHeader
+                    title={`${job.jobName} in ${job.location}`}
+                />
+                <CardMedia>
+                       
+                      <img src={`${job.image_url}`} alt="" />
+                </CardMedia>
+
+                <CardText>
+                    <h5> Job Description</h5>
+                    <div>
+                        {job.jobDescription}
+                    </div>
+                    <div>   
+                    <h5> Next Steps</h5>
+                      
+                        Job has been confirmed by the poster. 
+                        <br/>
+                        Post will contact you shortly.
+                        
+                    </div>
+                </CardText>
+            </Card>
+        )
+    }else{
+        return(
+    
+                <Card>
+
+                    <CardHeader
+                        title={`${job.jobName} in ${job.location}`}
+                    />
+                    <CardMedia>
+                           
+                          <img src={`${job.image_url}`} alt="" />
+                    </CardMedia>
+
+                    <CardText>
+                        <h5> Job Description</h5>
+                        <div>
+                            {job.jobDescription}
+                        </div>
+                    </CardText>
+                </Card>
+        )
+    }
+}
+
     render(){
         return(
             <div style={styles.root}>
                 <GridList
-                    cellHeight={60}
+                    cellHeight={300}
                     style={styles.gridList}
-                    cols={1}
-                    padding={0}
+                    cols={4}
+                    padding={3}
                 >
-                    {this.state.jobs.map((job) => {
-                        if (job.status == "confirmed" ) {
-                            return <GridTile
-                                        title={job.jobName}
-                                        titlePosition="top"
-                                        subtitle={job.currentApplicantEmail}
-                                        actionIcon={
-                                            <div>
-                                                <FlatButton label="Give Good Review" backgroundColor="#30F57B" primary={true} onClick={(event) => this.goodReview(event, job._id)}/>
-                                                <FlatButton label="Give Bad Review!" backgroundColor="#F53F30" primary={true} onClick={(event) => this.badReview(event, job._id)}/>
-                                            </div>
-                                        }
-                                    >
-                                    </GridTile>
-                        }
-                        else {
-                            return <span></span>
-                        }
-                    })}
+                    {this.state.jobs.length > 0? this.state.jobs.map((job) => this.createCard(job) ):<div>No Jobs found</div> }
                 </GridList>
             </div>
-
         )
     }
-
 }
+
+
+
+
+
+
+//     render(){
+//         return(
+//             <div style={styles.root}>
+//                 <GridList
+//                     cellHeight={60}
+//                     style={styles.gridList}
+//                     cols={1}
+//                     padding={0}
+//                 >
+//                     {this.state.jobs.map((job) => {
+//                         if (job.status == "confirmed" ) {
+//                             return <GridTile
+//                                         title={job.jobName}
+//                                         titlePosition="top"
+//                                         subtitle={job.currentApplicantEmail}
+//                                         actionIcon={
+//                                             <div>
+//                                                 <FlatButton label="Give Good Review" backgroundColor="#30F57B" primary={true} onClick={(event) => this.goodReview(event, job._id)}/>
+//                                                 <FlatButton label="Give Bad Review!" backgroundColor="#F53F30" primary={true} onClick={(event) => this.badReview(event, job._id)}/>
+//                                             </div>
+//                                         }
+//                                     >
+//                                     </GridTile>
+//                         }
+//                         else {
+//                             return <span></span>
+//                         }
+//                     })}
+//                 </GridList>
+//             </div>
+
+//         )
+//     }
+// }
 
 export default ConfirmedJobs;
